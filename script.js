52fe24f942d172d1983b6b4c12c1667c99eee32e
Jislainny Jessica <@Jislainny.j@escolar.ifrn.edu.br>
2023-01-26 11:47:39 -0300

    olimpiadasnews.2

---
+++ script.js
@@ -0,0 +1,13 @@
+//let entrada = prompt("Digite o ano que voce nasceu")
+//alert(2023 - entrada)
+//alert(typeof entrada)
+let section = document.querySelector("main");
+
+function toDarkSide() {
+  section.classList.add("dark");
+  section.classList.remove("ligth")
+}
+function toDarkSide() {
+    section.classList.add("dark");
+    section.classList.remove("ligth")
+  }
